5b040b624e9b7a402b3432f188b517820a7d8b2b
Christian Neciosup <cneiosuph@gmail.com>
2022-09-27 14:36:33 -0500

    creación de plantilla desde javascript

--- main.js
+++ main.js
@@ -1,8 +1,124 @@
-const menuEmail = document.querySelector('.navbar-email');
-const menu = document.querySelector('.desktop-menu');
+//ELEMENTOS OCULTOS CON LA CLASE "".inactive"
+const menuD = document.querySelector('.desktop-menu'); /* elemento que debemos mostrar*/
+const menuM = document.querySelector('.mobile-menu'); /* elemento que debemos mostrar */
+const carritoD = document.querySelector('.product-detail'); /* elemento que debemos mostrar */
 
-menuEmail.addEventListener('click', mostrar);
 
-function mostrar() {
-  menu.classList.toggle('inactive');
+const selectorMenuEmail = document.querySelector('.navbar-email');
+selectorMenuEmail.addEventListener('click', mostrarMenuDesktop);
+
+const selectorMenuMobile = document.querySelector('.menu');
+selectorMenuMobile.addEventListener('click',mostrarMenuMobile);
+
+const selectorCarrito = document.querySelector('#carrito');
+selectorCarrito.addEventListener('click', toggleCarrito);
+
+const cardsContainer = document.querySelector(".cards-container");
+
+
+//FUNCIONES PARA MOSTRAR U OCULTAR ELEMENTOS
+function mostrarMenuDesktop() 
+{
+  const isAsideCarritoClosed = carritoD.classList.contains("inactive");
+  if (!isAsideCarritoClosed) {
+    carritoD.classList.add("inactive");
+  }
+  menuD.classList.toggle('inactive');
+} 
+
+function mostrarMenuMobile()
+{
+ const isAsideCarritoClosed = carritoD.classList.contains('inactive');
+ if(!isAsideCarritoClosed){
+  carritoD.classList.add("inactive");
+ }
+  menuM.classList.toggle('inactive');
 }
+
+function toggleCarrito()
+{
+  const isMenuMobileClosed = menuM.classList.contains('inactive') ;
+  if(!isMenuMobileClosed){
+    menuM.classList.add('inactive');
+  }
+    carritoD.classList.toggle('inactive');
+}
+
+//CREAMOS UN ARRAY PARA PODER LLENAR LO INFO MANUALMENTE
+const productList = [];
+productList.push({
+  name: 'Bicicleta',
+  price: 320,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+  name: "Motocicleta",
+  price: 520,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Automovil",
+  price: 1200,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: "Camioneta",
+  price: 2400,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Scooter",
+  price: 1520,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Skate",
+  price: 1010,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+//CREAMOS LA PLANTILLA PARA CREAR LOS CARDS DESDE EL ARRAY
+for(product of productList){
+  const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+  const productImg = document.createElement('img');
+    productImg.setAttribute('src',product.image);
+
+  const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+  const productInfoDiv = document.createElement("div");
+
+  const productInfoPrice = document.createElement("p");
+    productInfoPrice.innerText = '$' + product.price;
+
+  const productInfoName = document.createElement("p");
+    productInfoName.innerText = product.name;
+
+  const productInfoFigure = document.createElement("figure");
+    const productImgCart = document.createElement("img");
+      productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+// ANIDAMOS LOS ELEMENTOS A SUS CONTENEDORES PADRE
+  productInfoFigure.appendChild(productImgCart);
+
+  productInfoDiv.appendChild(productInfoPrice);
+  productInfoDiv.appendChild(productInfoName);
+
+  productInfo.appendChild(productInfoDiv);
+  productInfo.appendChild(productInfoFigure);
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productInfo);
+
+  cardsContainer.appendChild(productCard);
+
+}
+
